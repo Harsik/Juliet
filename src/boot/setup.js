869bd8064a45b8cc067d79be38257082394ec668
aznm12 <aznm12@archivsoft.com>
2019-07-31 11:38:02 +0900

    reset

--- src/boot/setup.js
+++ src/boot/setup.js
@@ -1,16 +1,25 @@
-import React, { Component } from "react";
-import { StyleProvider } from "native-base";
+import React, { Component } from 'react'
+import { StyleProvider } from 'native-base'
 
-import App from "../App";
-import getTheme from "../theme/components";
-import variables from "../theme/variables/commonColor";
+import App from '../App'
+import getTheme from '../theme/components'
+import variables from '../theme/variables/commonColor'
 
 export default class Setup extends Component {
-  render() {
-    return (
-      <StyleProvider style={getTheme(variables)}>
-        <App />
-      </StyleProvider>
-    );
-  }
+	async componentDidMount() {
+		await Font.loadAsync({
+			Roboto: require('native-base/Fonts/Roboto.ttf'),
+			Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+			...Ionicons.font
+		})
+		this.setState({ isReady: true })
+	}
+
+	render() {
+		return (
+			<StyleProvider style={getTheme(variables)}>
+				<App />
+			</StyleProvider>
+		)
+	}
 }
